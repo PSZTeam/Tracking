ab65de6ea679641da282849c788579e4ac402365
NickwideN <deynega.nv@phystech.edu>
2019-12-21 20:21:43 +0300

    гироскоп

--- app/main.py
+++ app/main.py
@@ -1,15 +1,58 @@
-import kivy
-
-kivy.require('1.11.1')
-
+from kivy.lang import Builder
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.clock import Clock
+
+from plyer import accelerometer
 
 
-class DummyApp(App):
+class AccelerometerTest(BoxLayout):
+    def __init__(self):
+        super(AccelerometerTest, self).__init__()
+        self.sensorEnabled = False
+        self.f = open('data.csv', 'w')
+        self.f.write('X,Y,Z\n')
+
+    def do_toggle(self):
+        try:
+            if not self.sensorEnabled:
+                accelerometer.enable()
+                Clock.schedule_interval(self.get_acceleration, 1 / 20.)
+
+                self.sensorEnabled = True
+                self.ids.toggle_button.text = "Stop Accelerometer"
+            else:
+                accelerometer.disable()
+                Clock.unschedule(self.get_acceleration)
+                self.f.close()
+                self.sensorEnabled = False
+                self.ids.toggle_button.text = "Start Accelerometer"
+        except NotImplementedError:
+            import traceback
+            traceback.print_exc()
+            status = "Accelerometer is not implemented for your platform"
+            self.ids.accel_status.text = status
+
+    def get_acceleration(self, dt):
+        val = accelerometer.acceleration[:3]
+
+        if not val == (None, None, None):
+            self.ids.x_label.text = "X: " + str(val[0])
+
+            self.ids.y_label.text = "Y: " + str(val[1])
+            self.ids.z_label.text = "Z: " + str(val[2])
+
+            self.f.write(str(val[0]) + ',' + str(val[1]) + ',' + str(val[2]) + '\n')
+
+
+class AccelerometerTestApp(App):
     def build(self):
-        return Button(text="Hello World")
+        return AccelerometerTest()
+
+    def on_pause(self):
+        return True
 
 
 if __name__ == '__main__':
-    DummyApp().run()
+    AccelerometerTestApp().run()
